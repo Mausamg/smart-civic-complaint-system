6586146096c429733320538d92ad1847dd4e413a
Sandeep Shrestha <72135513+Sonseldeep@users.noreply.github.com>
2026-08-04 23:16:17 +0545

    Feat/documentation (#1)
    * add swagger support for documentation
    
    * add problem details and its middleware

--- Api/src/CivicComplaintSystem.Api/Program.cs
+++ Api/src/CivicComplaintSystem.Api/Program.cs
@@ -5,18 +5,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
+
+
 
 if (app.Environment.IsDevelopment())
 {
    app.UseSwagger();
-   
+   app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseExceptionHandler();
 
 app.MapControllers();
 
