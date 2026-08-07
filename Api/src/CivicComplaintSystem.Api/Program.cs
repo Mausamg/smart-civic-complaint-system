a8f4d95c8b01aa0ce6ecb6fbc264d178839099bd
Mausamg <mausamshrestha1200@gmail.com>
2026-08-07 21:07:30 +0545

    chore(api): configure project dependencies and middleware

--- Api/src/CivicComplaintSystem.Api/Program.cs
+++ Api/src/CivicComplaintSystem.Api/Program.cs
@@ -1,29 +1,29 @@
-
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
 
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddProblemDetails();
 
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-
+app.UseExceptionHandler();
 
 if (app.Environment.IsDevelopment())
 {
-   app.UseSwagger();
-   app.UseSwaggerUI();
-
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseExceptionHandler();
 
 app.MapControllers();
 
