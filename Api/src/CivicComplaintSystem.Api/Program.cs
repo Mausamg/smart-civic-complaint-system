6109e2ae6696869acdee2e55166f1d1c3220ef62
Mausamg <mausamshrestha1200@gmail.com>
2026-08-08 00:04:08 +0545

    feat(identity): add Identity configuration and migration

--- Api/src/CivicComplaintSystem.Api/Program.cs
+++ Api/src/CivicComplaintSystem.Api/Program.cs
@@ -1,30 +1,46 @@
+using CivicComplaintSystem.Api.Data;
+using CivicComplaintSystem.Api.Features.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddProblemDetails();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services
+    .AddIdentityCore<ApplicationUser>()
+    .AddRoles<IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
 var app = builder.Build();
-
-
 
 if (app.Environment.IsDevelopment())
 {
-   app.UseSwagger();
-   app.UseSwaggerUI();
-
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 app.UseExceptionHandler();
 
+app.UseAuthorization();
+
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    await IdentitySeeder.SeedIdentity(scope.ServiceProvider);
+}
 
 app.Run();
